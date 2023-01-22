193c4447b474955e346d5c4ef3c2fc8d9d01e007
Johnny <johnny_s.o@hotmail.com>
2023-01-22 12:24:06 -0300

    Adicionado component de Feed

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import './App.css'
 import Header from './components/Header/Header';
 import Main from './components/Main/Main';
+import Feed from './components/Feed/Feed';
 
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
     <div className="App">
       <Header />
       <Main />
+      <Feed />
     </div>
   )
 }
