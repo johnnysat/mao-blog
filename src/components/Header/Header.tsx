21d9106572773a0625e5d1282de840cc1a9307be
Johnny <johnny_s.o@hotmail.com>
2023-01-22 11:49:26 -0300

    Criando component Header e arquivo de CSS

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Header.css';
+
+export default function Header(){
+  return(
+    <>
+    <ul>
+      <li>MAO BLOG</li>
+      <li>Início </li>
+      <li>Sobre Mim</li>
+    </ul>
+    </>
+  )
+}
