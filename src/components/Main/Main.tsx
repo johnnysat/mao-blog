29128381431a64fbf3e4404eaa10088dece15666
Johnny <johnny_s.o@hotmail.com>
2023-01-23 06:51:50 -0300

    Adicionado grid ao component main

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -4,10 +4,10 @@ import './Main.css';
 export default function Main() {
   return(
     <div className="main__content">
-    <img className="main__img" src="https://fakeimg.pl/300x150/" />
-    <h1 className="main__title">Postagem Principal</h1>
-    <p className="main__resume">Um resumo da postagem principal, apenas uma parte do primeiro parágrafo. Algo a ser consider...
-    </p>
+      <img className="main__img" src="https://fakeimg.pl/400x200/" />
+      <h1 className="main__title">Postagem Principal</h1>
+      <p className="main__resume">Mussum Ipsum, cacilds vidis litro abertis. Casamentiss faiz malandris se pirulitá.Manduma pindureta quium dia nois paga.Mauris nec dolor in eros commodo tempor. Aenean aliquam molestie leo, vitae iaculis nisl.Quem num gosta di mim que vai caçá sua turmis!
+      </p>
     </div>
   )
 }
