2a29ca380d3391d9f5b8c78d4623d65b2e5b8b46
Johnny <johnny_s.o@hotmail.com>
2023-01-25 16:42:28 -0300

    Adicionado estilos ao Footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -4,7 +4,7 @@ import './Footer.css';
 export default function Footer(){
   return(
     <div>
-      <h2>Minhas redes sociais</h2>
+      <h2 className="footer__tittle">Minhas redes sociais</h2>
       <ul className="footer__list">
         <li>Tiktok</li>
         <li>Youtube</li>
